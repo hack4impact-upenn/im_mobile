d403ddedd89b767043b03389a98d125c8928c95a
sknop8 <sknop8@gmail.com>
2016-11-30 08:11:32 -0500

    title bar

--- RNApp/app/components/Map/Map.js
+++ RNApp/app/components/Map/Map.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Text, TouchableOpacity, ScrollView, View, Dimensions} from 'react-native';
 import styles from './styles';
 
+//TODO: fill in tile, topbar, bottombar components
+//      figure out how to import image for the map
+//      click navigation (back button, etc)
 const Map = (props) => {
-  const {text} = props;
+  const {country} = props;
   return (
     <ScrollView >
+        <View style={styles.topbar}>
+          <View style={styles.back} />
+          <View style={styles.topbarTextWrapper}> 
+            <Text style={styles.topbarText}>{country}</Text>
+          </View>
+        </View>
 
-      <View style={styles.container}>
-
+        <View style={styles.container}>
         <View style={styles.map} />
         <View style={styles.tile} />
         <View style={styles.tile} />
--- RNApp/app/components/Map/styles.js
+++ RNApp/app/components/Map/styles.js
@@ -8,16 +8,46 @@ export default StyleSheet.create({
     alignItems: 'center',
     backgroundColor: colors.background,
   },
+  topbar: {
+  	flex: 0,
+  	flexDirection:'row',
+  	width: Dimensions.get('window').width,
+    alignItems: 'center',
+  	backgroundColor: 'red',
+  	height: 50,
+  },
+  topbarText: {
+	fontSize: 25,
+  	fontWeight:'bold',
+  	color:'white',
+  	textAlign:'center',
+  },
+  topbarTextWrapper: {
+  	flex:0,
+  	width:Dimensions.get('window').width,
+  	alignItems:'center'
+  },
+  back: {
+  	position:'absolute',
+  	backgroundColor: 'orange',
+  	alignSelf:'flex-start',
+  	width:50,
+  	height:50,
+  },
   tile: {
-  	width: Dimensions.get('window').width * 0.8,
+  	alignSelf:'stretch',
   	backgroundColor: 'powderblue',
   	height: 120,
   	margin:20,
+  	marginLeft: 30,
+  	marginRight:30,
   },
   map: {
+  	alignSelf:'stretch',
   	backgroundColor: 'blue',
   	margin:20,
+  	marginLeft: 30,
+  	marginRight:30,
   	height: Dimensions.get('window').height * 0.4,
-  	width: Dimensions.get('window').width * 0.8,
   },
 });
