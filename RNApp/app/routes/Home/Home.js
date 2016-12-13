48380cc6a9149593343ed259cbcb96e4e74eb6a8
Anosha Minai <anoshaminai@hnt.1307.wlanwireless-pennnet.upenn.edu>
2016-12-13 14:54:03 -0500

    removed testing changes

--- RNApp/app/routes/Home/Home.js
+++ RNApp/app/routes/Home/Home.js
@@ -3,15 +3,15 @@ import { Text, View } from 'react-native';
 import Button from '../../components/Button';
 import styles from './styles';
 
-import SearchBar from '../../components/SearchBar';
-
 const Home = (props) => {
   return (
     <View style={styles.container}>
       <Text style={styles.main}>
         Home
       </Text>
-      <SearchBar
+      <Button
+        text="Details"
+        onPress={props.onDetailsPress}
       />
     </View>
   );
