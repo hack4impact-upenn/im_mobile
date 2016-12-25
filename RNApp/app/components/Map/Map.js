2f4180d70e734e4bd3eb1ef3d93834ba5c3e5cea
Suzanne Knop <sknop8@gmail.com>
2016-12-25 23:34:09 +0200

    Successfully retrieves geolocated data and displays country name & image

--- RNApp/app/components/Map/Map.js
+++ RNApp/app/components/Map/Map.js
@@ -5,77 +5,71 @@ import TopBar from './../TopBar';
 import Tile from './../Tile';
 import CountryCodes from './../../config/countryCodes';
 import Countries from './../../config/countries';
+import Loading from './../Loading'
 
 // import { makeRequest, getCountryName } from './../../backend/tempBackend';
 
 
 class Map extends Component {
   
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       isLoading: true,
       title: '',
-    };
+      iso3Code: '',
+    }; 
   }
 
-
   async makeRequest(countryCode) {
-      // Make request to Berkman API
-  // countryCode is in ISO3
+    // Make request to Berkman API
     const apiUrl = 'https://thenetmonitor.org/v2/countries/';
     let requestUrl = apiUrl + countryCode;
-    console.log('requesturl: ' + requestUrl);
     let data = '';
-    
     try {
       let response = await fetch(requestUrl);
       let responseJson = await response.json();
-      console.log('country name hereerererere');
-      console.log(responseJson['data']['attributes']['name']);
       this.setState({
         isLoading: false,
         title: responseJson['data']['attributes']['name']
       });
-      return;// responseJson['data'];
+      return responseJson['data'];
     } catch(error) {
       console.error(error);
     }
   }
 
   // TODO: Figure out how to import local images - right now we are importing from GitHub
-    getImageDir(code){
-      // return './../../images/country-icons/' + code.toLowerCase() + '.png';
-      return 'https://raw.githubusercontent.com/hack4impact/berkman/add-images/' +
-        'RNApp/app/images/country-icons/'+code.toLowerCase()+'.png';
-    }
-
-
+  getImageDir(code){
+    // return './../../images/country-icons/' + code.toLowerCase() + '.png';
+    return 'https://raw.githubusercontent.com/hack4impact/berkman/add-images/' +
+      'RNApp/app/images/country-icons/' + code.toLowerCase() + '.png';
+  }
 
   render() {
-    let imgUrl;
-     this.props.iso3Code = CountryCodes[this.props.iso2Code.toUpperCase()];
-    
+    let imgUrl;  
     // TODO: Create Loading component
     if (this.props.country == 'THE WORLD') {
         // TODO: Replace this with Internet Monitor world image
         imgUrl ='https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Simple_world_map.svg/2000px-Simple_world_map.svg.png'; 
-        this.state.title= 'the world';
+        this.state.title = 'the world';
       } else if (this.state.isLoading) {
-          let responseData = this.makeRequest(this.props.iso3Code.toLowerCase());
+
+        if (this.props.country != 'Unknown') {
+          this.state.iso3Code = CountryCodes[this.props.iso2Code.toUpperCase()];
+          let responseData = this.makeRequest(this.state.iso3Code.toLowerCase());
+          // TODO: Pass response data to tiles
+        }
 
         return (
           <View style={styles.container}>
             <TopBar title={''} back={false} />
-            <Text> 
-              Loading... 
-            </Text>
+              <Loading />
           </View>
 
         );
-    } else {
-       
-        imgUrl = 'https://thenetmonitor.org/countries/' + this.props.iso3Code.toLowerCase() + '/thumb';    
+    } else {   
+        imgUrl = 'https://thenetmonitor.org/countries/' + this.state.iso3Code.toLowerCase() + '/thumb';    
     }
 
     return (
@@ -108,12 +102,5 @@ Map.propTypes = {
   iso2Code: React.PropTypes.string,
   back: React.PropTypes.bool,
 };
-
-// Map.defaultProps = {
-//   country: 'United States',
-//   iso2Code: 'US',
-//   iso3Code: 'USA',
-//   back: false,
-// }
 
 export default Map;
