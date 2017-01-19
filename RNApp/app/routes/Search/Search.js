78457fd8d81f540dae0ecd9855eadf7506ed50ec
Siddharth Kumar <siddk@seas.upenn.edu>
2017-01-19 16:36:40 -0500

    Temp commit, checkout changed files

--- RNApp/app/routes/Search/Search.js
+++ RNApp/app/routes/Search/Search.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import SearchBar from '../../components/SearchBar';
-import Header from '../../components/Header';
+import Tile from '../../components/Tile'
 import styles from './styles';
+import CountryCodes from './../../config/countryCodes';
+import Countries from './../../config/countries';
+import CountryToId from './../../config/countryToId';
+
+
+
 
 const Search = (props) => {
+
+	const getImageDir = (code) => {
+    return 'https://raw.githubusercontent.com/hack4impact/berkman/add-images/' +
+      'RNApp/app/images/country-icons/'+code.toLowerCase()+'.png';
+  };
+
+   var allTilesArr = [];
+   var i;
+   i = -1;
+    for (var countryName in CountryToId){
+      var ccode = CountryToId[countryName];
+      
+      	i = i + 1;
+  		allTilesArr.push(
+   			<View key = {i}>
+  			<Tile titleText= {countryName} figureText= ' ' detailText= ' ' imageDir = {getImageDir(ccode)} tileType= 'country'/> 
+ 			</View>
+  		);
+  		
+ 	}
+ 
   return (
     <View style={styles.container}>
-      <Header/>
+
+    <ScrollView>
+
       <SearchBar
       />
+      	{allTilesArr}
+
+	</ScrollView>
+  
     </View>
   );
 };
