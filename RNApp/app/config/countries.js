b292ba831ceb72f7cf724eac850f72c1a355c7a9
Nancy Wong <nancypwong@gmail.com>
2017-01-25 20:34:33 -0500

    Add lat and long to countries list

--- RNApp/app/config/countries.js
+++ RNApp/app/config/countries.js
@@ -1,98 +1,467 @@
 /* Map country names to country numerical codes. */
 
 const countries = {
-  'united arab emirates':   784, 
-  'afghanistan':    4, 
-  'angola':   24, 
-  'austria':    40, 
-  'australia':   36, 
-  'azerbaijan':    31, 
-  'bangladesh':   50, 
-  'belgium':    56, 
-  'burkina faso':   854, 
-  'bulgaria':   100, 
-  'benin':   204, 
-  'bolivia':   68, 
-  'brazil':   76, 
-  'belarus':   112, 
-  'canada':   124, 
-  'switzerland':   756, 
-  'ivory coast':    384, 
-  'chile':    152, 
-  'china':    156, 
-  'colombia':   170, 
-  'czech republic':    203, 
-  'germany':   276, 
-  'denmark':   208, 
-  'dominican republic':   214, 
-  'algeria':     12, 
-  'ecuador':     218, 
-  'egypt':     818, 
-  'spain':     724, 
-  'ethiopia':   231, 
-  'finland':    246, 
-  'france':   250, 
-  'united kingdom':   826, 
-  'ghana':   288, 
-  'greece':    300, 
-  'guatemala':   320, 
-  'hong kong':   344, 
-  'honduras':    340, 
-  'haiti':   332, 
-  'hungary':    348, 
-  'indonesia':    360, 
-  'israel':    376, 
-  'india':    356, 
-  'iran':   364, 
-  'italy':    380, 
-  'jordan':   400, 
-  'japan':   392, 
-  'kenya':    404, 
-  'kyrgyzstan':    417, 
-  'cambodia':   116, 
-  'south korea':    410, 
-  'laos':   418, 
-  'sri lanka':   144, 
-  'morocco':   504, 
-  'madagascar':   450, 
-  'mali':   466, 
-  'mexico':    484, 
-  'malaysia':    458, 
-  'nigeria':    566, 
-  'nicaragua':    558, 
-  'netherlands':    528, 
-  'nepal':   524, 
-  'peru':    604, 
-  'philippines':   608, 
-  'pakistan':    586, 
-  'poland':    616, 
-  'portugal':    620, 
-  'paraguay':    600, 
-  'serbia':   688, 
-  'russia':    643, 
-  'saudi arabia':    682, 
-  'sudan':    729, 
-  'sweden':    752, 
-  'singapore':   702, 
-  'slovenia':    705, 
-  'slovakia':    703, 
-  'senegal':    686, 
-  'el salvador':     222, 
-  'syria':    760, 
-  'thailand':    764, 
-  'tajikistan':    762, 
-  'tunisia':    788, 
-  'taiwan':   158, 
-  'tanzania':    834, 
-  'ukraine':    804, 
-  'uganda':   800, 
-  'united states':    840, 
-  'uzbekistan':    860, 
-  'venezuela':   862, 
-  'vietnam':   704, 
-  'yemen':    887, 
-  'south africa':   710, 
-  'zambia':   894, 
+  'united arab emirates':   {
+    'numCode': 784, 
+    'lat': 23.4241,
+    'lng': 53.8478
+  },
+  'afghanistan': {
+    'numCode': 4, 
+    'lat': 33.93911,
+    'lng': 67.709953
+  },
+  'angola': {
+    'numCode': 24, 
+    'lat': -11.202692,
+    'lng': 17.873887
+  },
+  'austria': {
+    'numCode': 40, 
+    'lat': 47.516231,
+    'lng': 14.550072
+  },
+  'australia': {
+    'numCode': 36, 
+    'lat': -25.274398,
+    'lng': 133.775136
+  },
+  'azerbaijan': {
+    'numCode': 31, 
+    'lat': 40.143105,
+    'lng': 47.576927
+  },
+  'bangladesh': {
+    'numCode': 50, 
+    'lat': 23.684994,
+    'lng': 90.356331
+  },
+  'belgium': {
+    'numCode': 56, 
+    'lat': 50.503887,
+    'lng': 4.469936
+  },
+  'burkina faso': {
+    'numCode': 854, 
+    'lat': 12.238333,
+    'lng': -1.561593
+
+  },
+  'bulgaria': {
+    'numCode': 100, 
+    'lat': 42.733883,
+    'lng': 25.48583
+  },
+  'benin': {
+    'numCode': 204, 
+    'lat': 9.30769,
+    'lng': 2.315834
+  },
+  'bolivia': {
+    'numCode': 68, 
+    'lat': -16.290154,
+    'lng': -63.588653
+  },
+  'brazil': {
+    'numCode': 76, 
+    'lat': -14.235004,
+    'lng': -51.92528
+  },
+  'belarus': {
+    'numCode': 112, 
+    'lat': 53.709807,
+    'lng': 27.953389
+  },
+  'canada': {
+    'numCode': 124, 
+    'lat': 56.130366,
+    'lng': -106.346771
+  },
+  'switzerland': {
+    'numCode': 756, 
+    'lat': 46.818188,
+    'lng': 8.227512
+  },
+  'ivory coast': {
+    'numCode': 384, 
+    'lat': 7.539989,
+    'lng': -5.54708
+  },
+  'chile': {
+    'numCode': 152, 
+    'lat': -35.675147,
+    'lng': -71.542969
+  },
+  'china': {
+    'numCode': 156, 
+    'lat': 35.86166,
+    'lng': 104.195397
+  },
+  'colombia': {
+    'numCode': 170, 
+    'lat': 4.570868,
+    'lng': -74.297333
+  },
+  'czech republic': {
+    'numCode': 203, 
+    'lat': 49.817492,
+    'lng': 15.472962
+  },
+  'germany': {
+    'numCode': 276, 
+    'lat': 51.165691,
+    'lng': 10.451526
+  },
+  'denmark': {
+    'numCode': 208, 
+    'lat': 56.26392,
+    'lng': 9.501785
+  },
+  'dominican republic': {
+    'numCode': 214, 
+    'lat': 18.735693,
+    'lng': -70.162651
+  },
+  'algeria': {
+    'numCode': 12, 
+    'lat': 28.033886,
+    'lng': 1.659626
+  },
+  'ecuador': {
+    'numCode': 218, 
+    'lat': -1.831239,
+    'lng': -78.183406
+  },
+  'egypt': {
+    'numCode': 818, 
+    'lat': 26.820553,
+    'lng': 30.802498
+  },
+  'spain': {
+    'numCode': 724, 
+    'lat': 40.463667,
+    'lng': -3.74922
+  },
+  'ethiopia': {
+    'numCode': 231, 
+    'lat': 9.145,
+    'lng': 40.489673
+  },
+  'finland': {
+    'numCode': 246, 
+    'lat': 61.92411,
+    'lng': 25.748151
+  },
+  'france': {
+    'numCode': 250, 
+    'lat': 46.227638,
+    'lng': 2.213749
+  },
+  'united kingdom': {
+    'numCode': 826, 
+    'lat': 55.378051,
+    'lng': -3.435973
+  },
+  'ghana': {
+    'numCode': 288, 
+    'lat': 7.946527,
+    'lng': -1.023194
+  },
+  'greece': {
+    'numCode': 300, 
+    'lat': 39.074208,
+    'lng': 21.824312
+  },
+  'guatemala': {
+    'numCode': 320, 
+    'lat': 15.783471,
+    'lng': -90.230759
+  },
+  'hong kong': {
+    'numCode': 344, 
+    'lat': 22.396428,
+    'lng': 114.109497
+  },
+  'honduras': {
+    'numCode': 340, 
+    'lat': 15.199999,
+    'lng': -86.241905
+  },
+  'haiti': {
+    'numCode': 332, 
+    'lat': 18.971187,
+    'lng': -72.285215
+  },
+  'hungary': {
+    'numCode': 348, 
+    'lat': 47.162494,
+    'lng': 19.503304
+  },
+  'indonesia': {
+    'numCode': 360, 
+    'lat': -0.789275,
+    'lng': 113.921327
+  },
+  'israel': {
+    'numCode': 376, 
+    'lat': 31.046051,
+    'lng': 34.851612
+  },
+  'india': {
+    'numCode': 356, 
+    'lat': 20.593684,
+    'lng': 78.96288
+  },
+  'iran': {
+    'numCode': 364, 
+    'lat': 32.427908,
+    'lng': 53.688046
+  },
+  'italy': {
+    'numCode': 380, 
+    'lat': 41.87194,
+    'lng': 12.56738
+  },
+  'jordan': {
+    'numCode': 400, 
+    'lat': 30.585164,
+    'lng': 36.238414
+  },
+  'japan': {
+    'numCode': 392, 
+    'lat': 36.204824,
+    'lng': 138.252924
+  },
+  'kenya': {
+    'numCode': 404, 
+    'lat': -0.023559,
+    'lng': 37.906193
+  },
+  'kyrgyzstan': {
+    'numCode': 417, 
+    'lat': 41.20438,
+    'lng': 74.766098
+  },
+  'cambodia': {
+    'numCode': 116, 
+    'lat': 12.565679,
+    'lng': 104.990963
+  },
+  'south korea': {
+    'numCode': 410, 
+    'lat': 35.907757,
+    'lng': 127.766922
+  },
+  'laos': {
+    'numCode': 418, 
+    'lat': 19.85627,
+    'lng': 102.495496
+  },
+  'sri lanka': {
+    'numCode': 144, 
+    'lat': 7.873054,
+    'lng': 80.771797
+  },
+  'morocco': {
+    'numCode': 504, 
+    'lat': 31.791702,
+    'lng': -7.09262
+  },
+  'madagascar': {
+    'numCode': 450, 
+    'lat': -18.766947,
+    'lng': 46.869107
+  },
+  'mali': {
+    'numCode': 466, 
+    'lat': 17.570692,
+    'lng': -3.996166
+  },
+  'mexico': {
+    'numCode': 484, 
+    'lat': 23.634501,
+    'lng': -102.552784
+  },
+  'malaysia': {
+    'numCode': 458, 
+    'lat': 4.210484,
+    'lng': 101.975766
+  },
+  'nigeria': {
+    'numCode': 566, 
+    'lat': 9.081999,
+    'lng': 8.675277
+  },
+  'nicaragua': {
+    'numCode': 558, 
+    'lat': 12.865416,
+    'lng': -85.207229
+  },
+  'netherlands': {
+    'numCode': 528, 
+    'lat': 52.132633,
+    'lng': 5.291266
+  },
+  'nepal': {
+    'numCode': 524, 
+    'lat': 28.394857,
+    'lng': 84.124008
+  },
+  'peru': {
+    'numCode': 604, 
+    'lat': -9.189967,
+    'lng': -75.015152
+  },
+  'philippines': {
+    'numCode': 608, 
+    'lat': 12.879721,
+    'lng': 121.774017
+  },
+  'pakistan': {
+    'numCode': 586, 
+    'lat': 30.375321,
+    'lng': 69.345116
+  },
+  'poland': {
+    'numCode': 616, 
+    'lat': 51.919438,
+    'lng': 19.145136
+  },
+  'portugal': {
+    'numCode': 620, 
+    'lat': 39.399872,
+    'lng': -8.224454
+  },
+  'paraguay': {
+    'numCode': 600, 
+    'lat': -23.442503,
+    'lng': -58.443832
+  },
+  'serbia': {
+    'numCode': 688, 
+    'lat': 44.016521,
+    'lng': 21.005859
+  },
+  'russia': {
+    'numCode': 643, 
+    'lat': 61.52401,
+    'lng': 105.318756
+  },
+  'saudi arabia': {
+    'numCode': 682, 
+    'lat': 23.885942,
+    'lng': 45.079162
+  },
+  'sudan': {
+    'numCode': 729, 
+    'lat': 12.862807,
+    'lng': 30.217636
+  },
+  'sweden': {
+    'numCode': 752, 
+    'lat': 60.128161,
+    'lng': 18.643501
+  },
+  'singapore': {
+    'numCode': 702, 
+    'lat': 1.352083,
+    'lng': 103.819836
+  },
+  'slovenia': {
+    'numCode': 705, 
+    'lat': 46.151241,
+    'lng': 14.995463
+  },
+  'slovakia': {
+    'numCode': 703, 
+    'lat': 48.669026,
+    'lng': 19.699024
+  },
+  'senegal': {
+    'numCode': 686, 
+    'lat': 14.497401,
+    'lng': -14.452362
+  },
+  'el salvador': {
+    'numCode': 222, 
+    'lat': 13.794185,
+    'lng': -88.89653
+  },
+  'syria': {
+    'numCode': 760, 
+    'lat': 34.802075,
+    'lng': 38.996815
+  },
+  'thailand': {
+    'numCode': 764, 
+    'lat': 15.870032,
+    'lng': 100.992541
+  },
+  'tajikistan': {
+    'numCode': 762, 
+    'lat': 38.861034,
+    'lng': 71.276093
+  },
+  'tunisia': {
+    'numCode': 788, 
+    'lat': 33.886917,
+    'lng': 9.537499
+  },
+  'taiwan': {
+    'numCode': 158, 
+    'lat': 23.69781,
+    'lng': 120.960515
+  },
+  'tanzania': {
+    'numCode': 834, 
+    'lat': -6.369028,
+    'lng': 34.888822
+  },
+  'ukraine': {
+    'numCode': 804, 
+    'lat': 48.379433,
+    'lng': 31.16558
+  },
+  'uganda': {
+    'numCode': 800, 
+    'lat': 1.373333,
+    'lng': 32.290275
+  },
+  'united states': {
+    'numCode': 840, 
+    'lat': 37.09024,
+    'lng': -95.712891
+  },
+  'uzbekistan': {
+    'numCode': 860, 
+    'lat': 41.377491,
+    'lng': 64.585262
+  },
+  'venezuela': {
+    'numCode': 862, 
+    'lat': 6.42375,
+    'lng': -66.58973
+  },
+  'vietnam': {
+    'numCode': 704, 
+    'lat': 14.058324,
+    'lng': 108.277199
+  },
+  'yemen': {
+    'numCode': 887, 
+    'lat': 15.552727,
+    'lng': 48.516388
+  },
+  'south africa': {
+    'numCode': 710, 
+    'lat': -30.559482,
+    'lng': 22.937506
+  },
+  'zambia': {
+    'numCode': 894, 
+    'lat': -13.133897,
+    'lng': 27.849332
+  },
 };
 
 export default countries;
